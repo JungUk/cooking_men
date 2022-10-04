e3b64befea1bf24a214c254ae609d372e834861e
JungUk <62638035+JungUk@users.noreply.github.com>
2022-10-04 20:37:13 +0900

    testing

--- slider.js
+++ slider.js
@@ -1,42 +1,42 @@
-let curPos = 0;
-let position = 0;
-const IMAGE_WIDTH = 640;
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-const images = document.querySelector(".images");
+// let curPos = 0;
+// let position = 0;
+// const IMAGE_WIDTH = 640;
+// const prevBtn = document.querySelector(".prev");
+// const nextBtn = document.querySelector(".next");
+// const images = document.querySelector(".images");
 
-function prev() {
-  if(curPos > 0) {
-    nextBtn.removeAttribute("disabled");
-    position += IMAGE_WIDTH;
-    images.style.transform = `translateX(${position}px)`;
-    images.style.transition = `opacity 800ms ease-in-out 0s`;
-    curPos = curPos - 1;
-  }
+// function prev() {
+//   if(curPos > 0) {
+//     nextBtn.removeAttribute("disabled");
+//     position += IMAGE_WIDTH;
+//     images.style.transform = `translateX(${position}px)`;
+//     images.style.transition = `opacity 500ms ease-in-out`;
+//     curPos = curPos - 1;
+//   }
 
-  if(curPos == 0) {
-    prevBtn.setAttribute('disabled', 'true');
-  }
-}
+//   if(curPos == 0) {
+//     prevBtn.setAttribute('disabled', 'true');
+//   }
+// }
 
-function next() {
-  if(curPos < 3) {
-    prevBtn.removeAttribute("disabled");
-    position -= IMAGE_WIDTH;
-    images.style.transform = `translateX(${position}px)`;
-    images.style.transition = `background-color 0.3s ease-in-out 0s`;
-    curPos = curPos + 1;
-  }
+// function next() {
+//   if(curPos < 3) {
+//     prevBtn.removeAttribute("disabled");
+//     position -= IMAGE_WIDTH;
+//     images.style.transform = `translateX(${position}px)`;
+//     images.style.transition = `background-color 0.3s ease-in-out 0s`;
+//     curPos = curPos + 1;
+//   }
   
-  if(curPos == 3) {
-    nextBtn.setAttribute('disabled', 'true');
-  }
-}
+//   if(curPos == 3) {
+//     nextBtn.setAttribute('disabled', 'true');
+//   }
+// }
 
-function init() {
-  prevBtn.setAttribute('disabled', 'true');
-  prevBtn.addEventListener("click", prev);
-  nextBtn.addEventListener("click", next);
-}
+// function init() {
+//   prevBtn.setAttribute('disabled', 'true');
+//   prevBtn.addEventListener("click", prev);
+//   nextBtn.addEventListener("click", next);
+// }
 
-init();
+// init();
